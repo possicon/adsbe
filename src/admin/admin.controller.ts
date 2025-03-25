0deb8986e32a52fa4591f770919408420d96b221
Mac <mac@Macs-MacBook-Pro.local>
2025-03-25 13:28:34 +0100

    admin table

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,15 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  BadRequestException,
+  InternalServerErrorException,
+  ForbiddenException,
+  Req,
+  Query,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
-
+import { UserAuthGuard } from 'src/auth/guards/auth.guard';
+import { LoginDto } from 'src/auth/dto/login.dto';
+import { User } from 'src/auth/entities/auth.entity';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Post()
-  create(@Body() createAdminDto: CreateAdminDto) {
-    return this.adminService.create(createAdminDto);
+  @Post(':userId/Admin')
+  async makeFirstAdminUser(@Param('userId') userId: string) {
+    // const objectId = new Types.ObjectId(userId);
+    return this.adminService.makeFirstAdminUser(userId);
+  }
+  @Post('/login')
+  async adminLogin(@Body() loginDto: LoginDto) {
+    return this.adminService.adminLogin(loginDto);
+  }
+  @UseGuards(UserAuthGuard)
+  @Post(':userId/make-admin')
+  async ToggletoMakeUserAdmin(@Param('userId') userId: string, @Req() req) {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+
+    return this.adminService.ToggletoMakeUserAdmin(userId);
   }
 
   @Get()
@@ -17,18 +51,145 @@ export class AdminController {
     return this.adminService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.adminService.findOne(+id);
+  @Get('all/admins')
+  findAllAdmins() {
+    return this.adminService.findAllAdmins();
   }
 
+  @UseGuards(UserAuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
-    return this.adminService.update(+id, updateAdminDto);
+  async update(
+    @Param('id') id: string,
+    @Req() req,
+    @Body() updateAdminUserDto: UpdateAdminDto,
+  ) {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return this.adminService.update(id, updateAdminUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.adminService.remove(+id);
+  @UseGuards(UserAuthGuard)
+  @Delete(':userId/delete')
+  async remove(@Param('userId') id: string, @Req() req) {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    await this.adminService.remove(id);
+    return { message: 'Admin User has been removed' };
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.adminService.findById(id);
+  }
+  ///Admin and user
+  /////admin suspend user and soft delete user
+
+  @UseGuards(UserAuthGuard)
+  @Delete('delete/user/:userId')
+  async AdminRemoveUser(@Param('userId') userId: string, @Req() req) {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    await this.adminService.AdminRemoveUser(userId);
+    return { message: ' User has been removed' };
+  }
+  @UseGuards(UserAuthGuard)
+  @Patch(':userId/update/softdelete')
+  async softDeleteaUser(
+    @Param('userId') userId: string,
+
+    @Req() req,
+  ) {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return await this.adminService.softDeleteaUser(userId);
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Patch(':userId/update/suspend')
+  async suspendUser(
+    @Param('userId') userId: string,
+
+    @Req() req,
+  ) {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return await this.adminService.SuspendaUser(userId);
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Get('user/all/softdeleted')
+  async findAllSoftDeletedUser(
+    @Query() query: ExpressQuery,
+    @Req() req,
+  ): Promise<User[]> {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return this.adminService.findAllSoftDeletedUser(query);
+  }
+  @UseGuards(UserAuthGuard)
+  @Get('user/all/unsoftdeleted')
+  async findAllUnSoftDeletedUser(
+    @Query() query: ExpressQuery,
+    @Req() req,
+  ): Promise<User[]> {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return this.adminService.findAllNotSoftDeletedUser(query);
+  }
+  @UseGuards(UserAuthGuard)
+  @Get('user/all/suspended')
+  async findAllSuspendedUserr(
+    @Query() query: ExpressQuery,
+    @Req() req,
+  ): Promise<User[]> {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return this.adminService.findAllSuspendedUser(query);
+  }
+  @UseGuards(UserAuthGuard)
+  @Get('user/all/unsuspended')
+  async findAllUnSuspendedUserr(
+    @Query() query: ExpressQuery,
+    @Req() req,
+  ): Promise<User[]> {
+    const user = req.userId;
+    const adminAuthority = await this.adminService.getAdminByUserId(user);
+
+    if (adminAuthority.userId.toString() !== user) {
+      throw new ForbiddenException('Only admins can perform this action');
+    }
+    return this.adminService.findAllNotSuspendedUser(query);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,26 +1,339 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { Model, Types, UpdateQuery } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AdminUser } from './entities/admin.entity';
 
+import { Query } from 'express-serve-static-core';
+
+import { JwtService } from '@nestjs/jwt';
+import { v4 as uuidv4 } from 'uuid';
+import { LoginDto } from 'src/auth/dto/login.dto';
+import { RefreshToken } from 'src/auth/entities/refresh-token.schema';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/auth/entities/auth.entity';
 @Injectable()
 export class AdminService {
-  create(createAdminDto: CreateAdminDto) {
-    return 'This action adds a new admin';
+  constructor(
+    @InjectModel(AdminUser.name)
+    private readonly AdminUserModel: Model<AdminUser>,
+    @InjectModel(User.name) private UserModel: Model<User>,
+    @InjectModel(RefreshToken.name)
+    private RefreshTokenModel: Model<RefreshToken>,
+    private jwtService: JwtService,
+  ) {}
+  async makeFirstAdminUser(
+    // userId: Types.ObjectId
+    userId: string,
+  ): Promise<AdminUser> {
+    const userAlreadyAdmin = await this.AdminUserModel.findOne({
+      userId,
+    });
+    if (userAlreadyAdmin) {
+      throw new BadRequestException('This User is already an admin ');
+    }
+    const adminUser = new this.AdminUserModel({
+      userId: userId,
+      isAdmin: true,
+    });
+
+    return adminUser.save();
   }
 
-  findAll() {
-    return `This action returns all admin`;
+  async adminLogin(credentials: LoginDto) {
+    const { email, password } = credentials;
+
+    // Find the user by email
+    const user: any = await this.UserModel.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException('User Not found');
+    }
+
+    // Check if the user is an admin
+
+    const admin: any = await this.AdminUserModel.findOne({
+      userId: user._id.toString(),
+      isAdmin: true,
+    });
+    if (!admin || !admin.isAdmin) {
+      throw new UnauthorizedException('Access denied. Admin only');
+    }
+    console.log(admin);
+    // Validate password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Password mismatch');
+    }
+
+    // Generate JWT token
+    const payload = { userId: user._id, isAdmin: true, roles: admin.role };
+    const token = this.jwtService.sign(payload);
+
+    return {
+      token,
+      userId: user._id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      roles: admin.role,
+      isAdmin: admin.isAdmin,
+    };
+  }
+  async generateUserTokens(userId: any) {
+    const accessToken = this.jwtService.sign({ userId }, { expiresIn: '10h' });
+    const refreshToken = uuidv4();
+
+    await this.storeRefreshToken(refreshToken, userId);
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+  async storeRefreshToken(token: string, userId: string) {
+    // Calculate expiry date 3 days from now
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 3);
+
+    await this.RefreshTokenModel.updateOne(
+      { userId },
+      { $set: { expiryDate, token } },
+      {
+        upsert: true,
+      },
+    );
+  }
+  async findById(id: string): Promise<AdminUser> {
+    const category = await this.AdminUserModel.findById(id)
+      .populate({
+        path: 'userId',
+        select: '-password', // Exclude the password field
+      })
+      .exec();
+    if (!category) {
+      throw new NotFoundException('Admin User not found');
+    }
+    return category;
+  }
+  async findAdminByUserId(userId: string): Promise<AdminUser> {
+    const adminUser = await this.AdminUserModel.findOne({
+      userId,
+      isAdmin: true,
+    }).exec();
+
+    if (!adminUser) {
+      throw new NotFoundException('Admin User not found');
+    }
+
+    return adminUser;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} admin`;
+  async findAll(): Promise<AdminUser[]> {
+    return this.AdminUserModel.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: 'userId',
+        select: '-password', // Exclude the password field
+      })
+      .exec();
+  }
+  async findAllAdmins(): Promise<AdminUser[]> {
+    return this.AdminUserModel.find({ isAdmin: true })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: 'userId',
+        select: '-password', // Exclude the password field
+      })
+      .exec();
+  }
+  async findByUserId(userId: string): Promise<AdminUser> {
+    const adminUser = await this.AdminUserModel.findOne({ userId }).exec();
+
+    if (!adminUser) {
+      throw new NotFoundException('Admin User not found');
+    }
+
+    return adminUser;
+  }
+  async getAdminByUserId(userId: string): Promise<AdminUser> {
+    const adminuser = await this.AdminUserModel.findOne({
+      userId,
+      isAdmin: true,
+    }).exec();
+    if (!adminuser) {
+      throw new NotFoundException('Only admins can perform this action');
+    }
+    return adminuser;
   }
 
-  update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
+  async makeUserAdmin(
+    userId: string,
+
+    // isAdmin: boolean,
+  ): Promise<AdminUser> {
+    const user = await this.UserModel.findById({ _id: userId });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    const userAlreadyAdmin = await this.AdminUserModel.findOne({ userId });
+
+    const updatedAdmin = await this.AdminUserModel.findOneAndUpdate(
+      { userId },
+      { $set: { isAdmin: !userAlreadyAdmin.isAdmin || user } }, // Toggle between true and false
+      { new: true },
+    );
+
+    return updatedAdmin;
+  }
+  async ToggletoMakeUserAdmin(userId: string): Promise<AdminUser> {
+    const user = await this.UserModel.findById(userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    let adminUser = await this.AdminUserModel.findOne({ userId });
+
+    if (!adminUser) {
+      // If user is not in the admin collection, create an admin entry
+      adminUser = await this.AdminUserModel.create({
+        userId,
+        isAdmin: true,
+      });
+
+      // Also update the user model (assuming there is an isAdmin field)
+      await this.UserModel.findByIdAndUpdate(userId, { isAdmin: true });
+
+      return adminUser;
+    }
+
+    // If already in the admin collection, toggle isAdmin status
+    const newAdminStatus = !adminUser.isAdmin;
+
+    // Update the AdminUserModel
+    const updatedAdmin = await this.AdminUserModel.findOneAndUpdate(
+      { userId },
+      { $set: { isAdmin: newAdminStatus } },
+      { new: true },
+    );
+
+    // Update the UserModel accordingly
+    await this.UserModel.findByIdAndUpdate(userId, { isAdmin: newAdminStatus });
+
+    return updatedAdmin;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
+  async update(
+    id: string,
+    updateCategoryDto: Partial<UpdateAdminDto>,
+  ): Promise<AdminUser> {
+    const updateQuery: UpdateQuery<AdminUser> = updateCategoryDto;
+
+    const category = await this.AdminUserModel.findByIdAndUpdate(
+      id,
+      updateQuery,
+      {
+        new: true,
+      },
+    ).exec();
+
+    if (!category) {
+      throw new NotFoundException('Admin User not found');
+    }
+
+    return category;
   }
+
+  async remove(userId: string): Promise<void> {
+    const result = await this.AdminUserModel.findOneAndDelete({
+      userId: userId,
+    });
+    if (!result) {
+      throw new NotFoundException('Admin User not found');
+    }
+  }
+  ////Admin and users
+  async SuspendaUser(userId: string): Promise<User> {
+    const userDetails = await this.UserModel.findById({ _id: userId });
+    if (!userDetails) {
+      throw new NotFoundException('User not found');
+    }
+    userDetails.isSuspended = userDetails.isSuspended === false ? true : false;
+
+    // question.updatedAt = new Date();
+    return await userDetails.save();
+  }
+
+  async softDeleteaUser(userId: string): Promise<User> {
+    const userDetails = await this.UserModel.findById({ _id: userId });
+    if (!userDetails) {
+      throw new NotFoundException('User not found');
+    }
+    userDetails.isDeleted = userDetails.isDeleted === false ? true : false;
+
+    // question.updatedAt = new Date();
+    return await userDetails.save();
+  }
+  async findAllNotSoftDeletedUser(query: Query): Promise<User[]> {
+    const resPerPage = 10;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    return this.UserModel.find({
+      $or: [{ isDeleted: false }, { isDeleted: { $exists: false } }],
+    })
+      .sort({ createdAt: -1 })
+      .select('-password') // Exclude the password field
+      .limit(resPerPage)
+      .skip(skip)
+      .exec();
+  }
+  async findAllNotSuspendedUser(query: Query): Promise<User[]> {
+    const resPerPage = 10;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    return this.UserModel.find({
+      $or: [{ isSuspended: false }, { isSuspended: { $exists: false } }],
+    })
+      .sort({ createdAt: -1 })
+      .select('-password') // Exclude the password field
+      .limit(resPerPage)
+      .skip(skip)
+      .exec();
+  }
+  async findAllSoftDeletedUser(query: Query): Promise<User[]> {
+    const resPerPage = 10;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    return this.UserModel.find({ isDeleted: true })
+      .sort({ createdAt: -1 })
+      .select('-password') // Exclude the password field
+      .limit(resPerPage)
+      .skip(skip)
+      .exec();
+  }
+  async findAllSuspendedUser(query: Query): Promise<User[]> {
+    const resPerPage = 10;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    return this.UserModel.find({ isSuspended: true })
+      .sort({ createdAt: -1 })
+      .select('-password') // Exclude the password field
+      .limit(resPerPage)
+      .skip(skip)
+      .exec();
+  }
+  async AdminRemoveUser(userId: string): Promise<void> {
+    const result = await this.UserModel.findOneAndDelete({
+      userId: userId,
+    });
+    if (!result) {
+      throw new NotFoundException(' User not found');
+    }
+  }
+  ////admin dashboard
 }
