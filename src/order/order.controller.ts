af35fca2b59560cfeb5ed50320481f2c31b9a3d1
Mac <mac@Macs-MacBook-Pro.local>
2025-04-28 13:17:02 +0100

    login user approved and not approved  orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -54,6 +54,31 @@ export class OrderController {
     return this.orderService.findAllNotApprovedOrderPagination(query);
   }
 
+  @UseGuards(UserAuthGuard)
+  @Get('approved/all/user')
+  async getLoginUserAllApprovedOrdersPagination(
+    @Query() query: ExpressQuery,
+    @Req() req,
+  ): Promise<Order[]> {
+    const userId = req.userId;
+    return this.orderService.findLoginUserAllApprovedOrderPagination(
+      query,
+      userId,
+    );
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Get('notapproved/all/user')
+  async getLoginUserAllNotApprovedOrdersPagination(
+    @Query() query: ExpressQuery,
+    @Req() req,
+  ): Promise<Order[]> {
+    const userId = req.userId;
+    return this.orderService.findLoginUserAllNotApprovedOrderPagination(
+      query,
+      userId,
+    );
+  }
   @Get('pag/all')
   async getAllOrdersPagination(@Query() query: ExpressQuery): Promise<Order[]> {
     return this.orderService.findAllOrderPagination(query);
